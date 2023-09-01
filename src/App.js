928e5a856a2515a8cd979062b2cd312b7f9440d0
Adam-Ratajczak <99399261+Adam-Ratajczak@users.noreply.github.com>
2023-09-01 18:58:58 +0200

    Checks (kinda) working

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.scss';
 
 const lines_of_code = 20
 
-const Level = {
-  command: "You have three variables given: a, b and c. Calculate 10th element of the fibonacci sequence where a should be an result.",
-  code:"// write your code here\nlet a = 1;\nlet b = 0;\nlet c = 0;\n"
+let Level = {
+  command: "You have three variables given: a, b and c. Calculate 10th element of the fibonacci sequence where a should be a result. Print a result in the console.",
+  code:"// write your code here\nlet a = 1;\nlet b = 0;\nlet c = 0;\n",
+  checks: [
+    {msg: "Is 'a' variable valid?", eval: "a == 89"},
+    {msg: "Is value printed in the console?", eval: "consoleLogs.Length == 1 && consoleLogs[0].value == \"89\""}
+  ],
+  page_content: false
 }
 
 function App() {
   let line_div_arr = []
+
+  const [CurrLevel, SetCurrLevel] = useState(0)
 
   for(let i = 1; i <= lines_of_code; i++){
     line_div_arr.push((
@@ -18,6 +25,7 @@ function App() {
   }
 
   const [ConsoleEntry, SetConsoleEntry] = useState([])
+  const [Checks, SetChecks] = useState([])
 
   function limitLines(e) {
     const str = document.getElementById("code-content").value
@@ -44,8 +52,20 @@ function App() {
     }
   }
 
+  useState(()=>{
+    SetCurrLevel(Level)
+
+    let check_arr = []
+
+    for(let check of Level.checks){
+      check_arr.push((<div style={{backgroundColor: "#FFFFFF88"}}>{check.msg}</div>))
+    }
+
+    SetChecks(check_arr)
+  }, [1])
+
   function execute(e){
-    const str = document.getElementById("code-content").value
+    let code = document.getElementById("code-content").value
     console.clear()
     var consoleLogs = [];
 
@@ -53,25 +73,31 @@ function App() {
     var originalConsoleLog = console.log;
     console.log = function() {
       originalConsoleLog.apply(console, arguments);
-      consoleLogs.push({type: "log", value: Array.from(arguments)});
+      consoleLogs.push({type: "log", value: arguments[0].toString()});
     };
     var originalConsoleError = console.error;
     console.error = function() {
       originalConsoleError.apply(console, arguments);
-      consoleLogs.push({type: "error", value: Array.from(arguments)});
+      consoleLogs.push({type: "error", value: arguments[0].toString()});
     };
     var originalConsoleWarn = console.warn;
     console.warn = function() {
       originalConsoleWarn.apply(console, arguments);
-      consoleLogs.push({type: "warn", value: Array.from(arguments)});
+      consoleLogs.push({type: "warn", value: arguments[0].toString()});
     };
     var originalConsoleDebug = console.debug;
     console.debug = function() {
       originalConsoleDebug.apply(console, arguments);
-      consoleLogs.push({type: "debug", value: Array.from(arguments)});
+      consoleLogs.push({type: "debug", value: arguments[0].toString()});
     };
 
-    eval(str)
+    const foo = new Function(code + "\nreturn [a, b, c];")
+
+    const res = foo()
+
+    const a = res[0]
+    const b = res[1]
+    const c = res[2]
 
     console.log = originalConsoleLog;
     console.error = originalConsoleError;
@@ -82,23 +108,36 @@ function App() {
 
     for(let entry of consoleLogs){
       if(entry.type == "log"){
-        arr.push((<div style={{color: "black"}}>{" > " + entry.value[0]}</div>))
+        arr.push((<div style={{color: "black"}}>{" > " + entry.value}</div>))
       }else if(entry.type == "error"){
-        arr.push((<div style={{color: "red"}}>{" > " + entry.value[0]}</div>))
+        arr.push((<div style={{color: "red"}}>{" > " + entry.value}</div>))
       }else if(entry.type == "warn"){
-        arr.push((<div style={{color: "gold"}}>{" > " + entry.value[0]}</div>))
+        arr.push((<div style={{color: "gold"}}>{" > " + entry.value}</div>))
       }else if(entry.type == "debug"){
-        arr.push((<div style={{color: "silver"}}>{" > " + entry.value[0]}</div>))
+        arr.push((<div style={{color: "silver"}}>{" > " + entry.value}</div>))
       }
     }
 
+    let check_arr = [];
+    for (let check of CurrLevel.checks) {
+      // Add checks to ensure that 'a', 'b', 'c', and consoleLogs[0] are defined
+      if (a !== undefined && b !== undefined && c !== undefined) {
+        const bar = new Function("a", "b", "c", "consoleLogs", "return " + check.eval + ";");
+        const result = bar(a, b, c, consoleLogs);
+    
+        const divStyle = result ? { backgroundColor: "#00FF0088" } : { backgroundColor: "#FF000088" };
+        check_arr.push(<div style={divStyle}>{check.msg}</div>);
+      }
+    }
+
+    SetChecks(check_arr)
     SetConsoleEntry(arr)
   }
 
   return (
     <div id="main">
       <div id="command">
-        {Level.command}
+        {CurrLevel.command}
       </div>
       <div id="content">
         <div id="code">
@@ -113,21 +152,23 @@ function App() {
               {line_div_arr}
             </div>
             <textarea id="code-content" rows={lines_of_code} onKeyDown={(ev) => limitLines(ev)}>
-              {Level.code}
+              {CurrLevel.code}
             </textarea>
           </div>
         </div>
-        <div id="console">
-          <div id="console-header">
-            Console:
+        {CurrLevel.page_content ? CurrLevel.page_content : (
+          <div id="console">
+            <div id="console-header">
+              Console:
+            </div>
+            <div id="console-content">
+              {ConsoleEntry}
+            </div>
           </div>
-          <div id="console-content">
-            {ConsoleEntry}
-          </div>
-        </div>
+        )}
       </div>
       <div id="checks">
-
+          {Checks}
       </div>
     </div>
   );
