4d9df262003d5614373d95831a217452b951b829
Adam-Ratajczak <99399261+Adam-Ratajczak@users.noreply.github.com>
2023-09-02 11:16:34 +0200

    Exercises 1-6

--- src/App.js
+++ src/App.js
@@ -4,39 +4,13 @@ import './App.scss';
 const lines_of_code = 20
 
 const Levels = [
-  {
-    command: "You have three variables given: a, b and c. Calculate 10th element of the fibonacci sequence where a should be a result. Print a result in the console.",
-    code:"let a = 1;\nlet b = 0;\nlet c = 0;\n// write your code here\n",
-    protected_lines: 3,
-    vars: "return [a, b, c];",
-    checks: [
-      {msg: "Is 'a' variable valid?", eval: "return res[0] == 89"},
-      {msg: "Is value printed in the console?", eval: "for(let cmd of consoleLogs){if(cmd.value == res[0]){ return true;}}\nreturn false;"}
-    ],
-    page_content: false
-  },
-  {
-    command: "You have three variables given: a, b and c. Calculate 10th element of the fibonacci sequence where a should be a result. Print a result in the console.",
-    code:"let a = 1;\nlet b = 0;\nlet c = 0;\n// write your code here\n",
-    protected_lines: 3,
-    vars: "return [a, b, c];",
-    checks: [
-      {msg: "Is 'a' variable valid?", eval: "return res[0] == 89"},
-      {msg: "Is value printed in the console?", eval: "for(let cmd of consoleLogs){if(cmd.value == res[0]){ return true;}}\nreturn false;"}
-    ],
-    page_content: (<div id="sample-page"><p>bajo jajo</p></div>)
-  },
-  {
-    command: "You have three variables given: a, b and c. Calculate 10th element of the fibonacci sequence where a should be a result. Print a result in the console.",
-    code:"let a = 1;\nlet b = 0;\nlet c = 0;\n// write your code here\n",
-    protected_lines: 3,
-    vars: "return [a, b, c];",
-    checks: [
-      {msg: "Is 'b' variable valid?", eval: "return res[0] == 89"},
-      {msg: "Is value printed in the console?", eval: "for(let cmd of consoleLogs){if(cmd.value == res[0]){ return true;}}\nreturn false;"}
-    ],
-    page_content: (<div id="sample-page"><p>bajo jajo</p></div>)
-  },
+  {command: "You have a variable 'CurrDate' given. Write a JavaScript program to display the current day and time in the UTC. Write the result in the console.", code: "let CurrDate = new Date();\n// write your code here\n", protected_lines: 1, vars: "return [CurrDate];", checks: [ {msg: "Is the result printed in the console?", eval: "for(let cmd of consoleLogs){if(cmd.type == 'log' && cmd.value.includes(res[0].toUTCString())){ return true;}}\nreturn false;", }, ], page_content: false},
+  {command: "You have given a variable: 'Area'. Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7. Write the result in the console rounded to 2 decimal digits.", code: "let Area = 0;\n// write your code here\n", protected_lines: 1, vars: "return [Area]", checks: [ { msg: "Is 'Area' variable valid for the 5, 6, 7 triangle?", eval: "return Math.abs(res[0] - 14.6969384) < 0.001;", }, { msg: "Is the rounded value of 'Area' written in the console?", eval: "let roundedArea = Math.round(res[0] * 100) / 100;\nfor (let cmd of consoleLogs) { if (cmd.value.includes(roundedArea.toString())) { return true; }}\nreturn false;", }, ], page_content: false, },
+  {command: "Write a JavaScript program to rotate the string 'I hate JavaScript' in the right direction. This is done by periodically removing one letter from the string end and attaching it to the front. Write the result to the given 'result' variable.", code: "const inputValue = 'I hate JavaScript';\nlet result = '';\n// write your code here\n", protected_lines: 2, vars: "return [result];", checks: [ { msg: "Does 'result' have the correct rotated value?", eval: "return res[0] === 'tpircSavaJ etah I';", }, ], page_content: false, },
+  {command: "You have given input values in an array: [2000, 2012, 1993, 1900]. Write a JavaScript program to determine whether a given year is a leap year in the Gregorian calendar. Iterate the array and add to 'result' a string 'Given year is a leap year' if true, otherwise, 'Given year is not a leap year'.", code: "const years = [2000, 2012, 1993, 1900];\nlet result = [];\n// write your leap year determination logic here\n", protected_lines: 2, vars: "return [result, years];", checks: [ { msg: "Is 'result' correct for leap year determination?", eval: `return Array.isArray(res[0]) && res[0].length === res[1].length && res[0].every((value, index) => { const year = res[1][index]; if (value === 'Given year is a leap year') { return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0); } else { return !(year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)); } } );`, }, ], page_content: false, },
+  {command: "Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050. Write all found values in the 'result' variable.", code: "let result = [];\n// write your code here\n", protected_lines: 1, vars: "return [result];", checks: [ { msg: "Is 'result' an array containing years when 1st January is a Sunday?", eval: `return Array.isArray(res[0]) && res[0].every(year => { const januaryFirst = new Date(year, 0, 1); return januaryFirst.getDay() === 0 && year >= 2014 && year <= 2050; });`, }, ], page_content: false, },
+  {command: "Write a JavaScript program where the program takes a random integer between 1 and 10, and the user is then prompted to input a guess number. The program prints 'Good Work' to the console if the input matches the guess number; otherwise, it prints 'Not matched'. The randomly generated number should be written to the given 'num' variable, and the user prompt should be written to the 'prompt' variable.", code: "let num = 0;\nlet prompt = 0;\n// write your code here\n", protected_lines: 2, vars: "return [num, prompt];", checks: [ { msg: "Is the 'num' variable a random integer between 1 and 10?", eval: "return Number.isInteger(res[0]) && res[0] >= 1 && res[0] <= 10;", }, { msg: "Is value printed in the console?", eval: "for(let cmd of consoleLogs){if(cmd.value == (res[0] === res[1] ? 'Good Work' : 'Not matched')){ return true;}}\nreturn false;", }, ], page_content: false,}
+  
 ]
 
 function App() {
@@ -93,6 +67,10 @@ function App() {
   function ChangeLevel(id){
     if(id < 0 || id >= Levels.length){
       return
+    }
+
+    if(document.getElementById("code-content")){
+      document.getElementById("code-content").value = Levels[id].code
     }
 
     SetCurrLevel(Levels[id])
